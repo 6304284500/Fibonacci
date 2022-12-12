7a490ebc837f8a3d7042c1387973eb464a8ccd33
Kolavennu Sai Sreeram Kumar <sensational.sreeram999@gmail.com>
2022-12-12 23:52:04 +0530

    commit message

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public int len;
+    
+   static void Main(string[] args)
+   {
+
+        int a = 0, b = 1, c = 0;
+        Console.Write("{0} {1}", a, b);
+        for (int i = 2; i < len; i++)
+        {
+            c = a + b;
+            Console.Write(" {0}", c);
+            a = b;
+            b = c;
+        }
+     }
+}
+  
